3c399c5d2da9d670919b4f39ad7eaa97adcdc329
Deepak Dhanoliya <ddhakad910@gmail.com>
2025-12-03 14:06:12 +0530

    commit

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,11 @@ const User = require("./models/User");
 const Task = require("./models/Task");
 
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin: "*",
+  methods: "GET,POST,PUT,DELETE",
+  allowedHeaders: "Content-Type,Authorization"
+}));
 app.use(express.json());
 
 // DB connect
